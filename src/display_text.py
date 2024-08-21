953fe343492f45418172266e0901f654ceb8eed6
Antoine Montes <antoine.montes@pickyourskills.com>
2024-08-21 21:50:33 +0200

    factorize

---
+++ src/display_text.py
@@ -0,0 +1,28 @@
+from napta_matrix import RGBMatrix, graphics, matrix_script
+from pathlib import Path
+import time
+
+
+@matrix_script
+def display_text(matrix: RGBMatrix) -> None:
+    offscreen_canvas = matrix.CreateFrameCanvas()
+
+    font = graphics.Font()
+    font_path = Path(__file__).parent.resolve() / "../fonts/7x13.bdf"
+    font.LoadFont(str(font_path.resolve()))
+
+    text_color = graphics.Color(255, 255, 0)
+    pos = offscreen_canvas.width
+    my_text = "Hello Napta!"
+    while True:
+        offscreen_canvas.Clear()
+        len = graphics.DrawText(offscreen_canvas, font, pos, 10, text_color, my_text)
+        pos -= 1
+        if pos + len < 0:
+            pos = offscreen_canvas.width
+        time.sleep(0.05)
+        offscreen_canvas = matrix.SwapOnVSync(offscreen_canvas)
+
+
+if __name__ == "__main__":
+    display_text()
--- src/napta_matrix.py
+++ src/napta_matrix.py
@@ -1,12 +1,34 @@
+from collections.abc import Callable
 import os
+
+
+MATRIX_SIZE = 64
 
 
 def is_raspberry() -> bool:
     """Check if the current device is the Raspberry Pi."""
     return os.uname().machine == 'armv6l'
 
+
+
 if is_raspberry():
     from rgbmatrix import RGBMatrix, RGBMatrixOptions, graphics
 else:
     from RGBMatrixEmulator import RGBMatrix, RGBMatrixOptions, graphics
 
+
+
+def matrix_script(function: Callable[[RGBMatrix], None]) -> Callable[[], None]:
+    def wrapper() -> None:
+        options = RGBMatrixOptions()
+        options.rows = MATRIX_SIZE
+        options.cols = MATRIX_SIZE
+        options.chain_length = 1
+        options.parallel = 1
+        options.hardware_mapping = "regular"
+
+        matrix = RGBMatrix(options=options)
+
+        function(matrix)
+    
+    return wrapper
