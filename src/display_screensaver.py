f957d1e9d5ea728678ef8e5b4cd3ddc64069d1dc
Loïc Simon <loic.simon@napta.io>
2024-08-22 16:21:29 +0200

    Make screensaver not a square

--- src/display_screensaver.py
+++ src/display_screensaver.py
@@ -5,13 +5,19 @@ import time
 from napta_matrix import RGBMatrix, matrix_script
 
 NAPTA_GREEN = (9, 203, 156)
-LOGO_SIZE = 12
+NAPTA_SPRAY = (117, 221, 221)
+NAPTA_INDIGO = (84, 5, 255)
+NAPTA_GORSE = (249, 229, 64)
+RED = (255, 0, 0)
+
+LOGO_HEIGHT = 12
+LOGO_WIDTH = 16
 
 @matrix_script
 def display_image(matrix: RGBMatrix) -> None:
-    image = Image.new("RGB", (LOGO_SIZE, LOGO_SIZE))
+    image = Image.new("RGB", (LOGO_WIDTH, LOGO_HEIGHT))
     draw = ImageDraw.Draw(image)
-    # draw.rectangle((0, 0, 64, 64), fill=(0, 55, 85), outline=(0, 0, 255))
+    # Napta
     draw.line((0, 1, 0, 11), fill=NAPTA_GREEN)
     draw.line((1, 0, 1, 11), fill=NAPTA_GREEN)
     draw.line((2, 0, 2, 11), fill=NAPTA_GREEN)
@@ -24,12 +30,24 @@ def display_image(matrix: RGBMatrix) -> None:
     draw.line((9, 0, 9, 11), fill=NAPTA_GREEN)
     draw.line((10, 0, 10, 11), fill=NAPTA_GREEN)
     draw.line((11, 0, 11, 10), fill=NAPTA_GREEN)
+    # L
+    draw.line((13, 0, 13, 0), fill=NAPTA_SPRAY)
+    draw.line((13, 1, 13, 1), fill=NAPTA_SPRAY)
+    draw.line((13, 2, 15, 2), fill=NAPTA_SPRAY)
+    # E
+    draw.line((13, 3, 15, 3), fill=NAPTA_INDIGO)
+    draw.line((13, 4, 13, 4), fill=NAPTA_INDIGO)
+    draw.line((13, 5, 14, 5), fill=NAPTA_INDIGO)
+    draw.line((13, 6, 13, 6), fill=NAPTA_INDIGO)
+    draw.line((13, 7, 15, 7), fill=NAPTA_INDIGO)
+    # D
+    draw.line((13, 8, 14, 8), fill=NAPTA_GORSE)
+    draw.line((13, 9, 13, 10), fill=NAPTA_GORSE)
+    draw.line((15, 9, 15, 10), fill=NAPTA_GORSE)
+    draw.line((13, 11, 14, 11), fill=NAPTA_GORSE)
 
-    matrix.Clear()
-    matrix.SetImage(image)
-
-    x = randrange(64 - LOGO_SIZE)
-    y = randrange(64 - LOGO_SIZE)
+    x = randrange(64 - LOGO_WIDTH)
+    y = randrange(64 - LOGO_HEIGHT)
     dx = choice([-1, 1])
     dy = choice([-1, 1])
 
@@ -42,14 +60,14 @@ def display_image(matrix: RGBMatrix) -> None:
         if x < 0:
             x = 0
             dx = -dx
-        if x > 64 - LOGO_SIZE:
-            x = 64 - LOGO_SIZE
+        if x > 64 - LOGO_WIDTH:
+            x = 64 - LOGO_WIDTH
             dx = -dx
         if y < 0:
             y = 0
             dy = -dy
-        if y > 64 - LOGO_SIZE:
-            y = 64 - LOGO_SIZE
+        if y > 64 - LOGO_HEIGHT:
+            y = 64 - LOGO_HEIGHT
             dy = -dy
 
 
