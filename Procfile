d44ec4f21d3b37eebc26c750370a1cb0084f772f
Antoine Montes <antoine.montes@pickyourskills.com>
2024-08-28 15:19:03 +0200

    add client

--- Procfile
+++ Procfile
@@ -1,1 +1,2 @@
 worker: sudo python3 -m uvicorn app:app --host 0.0.0.0 --port 8042
+static: /:client/dist
---
+++ client/src/App.tsx
@@ -0,0 +1,7 @@
+import "./App.css";
+
+function App() {
+  return <div>NaptALED</div>;
+}
+
+export default App;
