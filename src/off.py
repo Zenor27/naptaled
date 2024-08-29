933b64822746d8f44f00fad423eb75b9419347de
Alexis Leveque <alexis.leveque@napta.io>
2024-08-29 19:18:28 +0200

    Add a off script for deployment

---
+++ src/off.py
@@ -0,0 +1,22 @@
+import asyncio
+
+from PIL import Image, ImageDraw
+
+from src.helpers.fullscreen_message import fullscreen_message
+from src.napta_matrix import RGBMatrix, matrix_script
+
+
+@matrix_script
+async def off(matrix: RGBMatrix) -> None:
+    fullscreen_message(
+        matrix,
+        [
+            "Deployment",
+            "    in    ",
+            "progress!",
+        ],
+    )
+
+
+if __name__ == "__main__":
+    asyncio.run(display_image())
