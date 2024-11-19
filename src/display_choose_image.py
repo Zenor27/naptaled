1233994318da08f8403647a90af865ecec221622
Loïc Simon <loic.simon@napta.io>
2024-11-19 14:24:39 +0100

    Clean

--- src/display_choose_image.py
+++ src/display_choose_image.py
@@ -28,11 +28,11 @@ def loader_screen_range(matrix: RGBMatrix, n_steps: int) -> Iterator[int]:
     graphics.DrawText(loader_buffer, font, 49, 25, graphics.Color(*NaptaColor.CORN_FIELD), "IF")
 
     X_MIN = 5
-    X_MAX = MATRIX_SIZE - 7
+    X_MAX = MATRIX_SIZE - 8
     Y_MIN = 30
     Y_MAX = 40
 
-    for x in range(X_MIN, X_MAX):
+    for x in range(X_MIN, X_MAX + 1):
         loader_buffer.SetPixel(x, Y_MIN, *NaptaColor.CORN_FIELD)
         loader_buffer.SetPixel(x, Y_MAX, *NaptaColor.CORN_FIELD)
     for y in range(Y_MIN, Y_MAX + 1):
@@ -41,11 +41,12 @@ def loader_screen_range(matrix: RGBMatrix, n_steps: int) -> Iterator[int]:
 
     matrix.SwapOnVSync(loader_buffer)
 
+    progress_size = (X_MAX - X_MIN - 1)
     old_progress = 0
     for step in range(n_steps):
         yield step
 
-        progress = math.ceil((step / n_steps) * (X_MAX - X_MIN))
+        progress = math.ceil((step / n_steps) * progress_size)
         for new_progress in range(old_progress, progress):
             for y in range(Y_MIN + 1, Y_MAX):
                 loader_buffer.SetPixel(X_MIN + new_progress + 1, y, *NaptaColor.GREEN)
@@ -69,21 +70,21 @@ async def display_choose_image(
 
     if n_frames > 1:
         # Load GIF frames in memory
-        buffers = list[tuple[Any, float]]()
+        frames = list[tuple[Any, float]]()
         for keyframe in loader_screen_range(matrix, n_steps=n_frames):
             image_obj.seek(keyframe)
             frame_buffer = matrix.CreateFrameCanvas()
             frame_buffer.SetImage(get_frame(image_obj))
             duration_in_ms = image_obj.info["duration"]
-            buffers.append((frame_buffer, duration_in_ms))
+            frames.append((frame_buffer, duration_in_ms / 1000))
 
         # Display frames
         while True:
-            for buffer, duration_in_ms in buffers:
+            for frame_buffer, frame_duration in frames:
                 t0 = time()
-                matrix.SwapOnVSync(buffer)
+                matrix.SwapOnVSync(frame_buffer)
                 elapsed_time = time() - t0
-                await asyncio.sleep(duration_in_ms / 1000 - elapsed_time)
+                await asyncio.sleep(frame_duration - elapsed_time)
 
     else:
         # For static images, just display them
